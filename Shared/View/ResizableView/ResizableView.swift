c014022eeeba9afb7b8892e7d720b1c257f8438e
brown <xenix72@gmail.com>
2021-07-12 17:50:21 +0900

    check

--- Shared/View/ResizableView/ResizableView.swift
+++ Shared/View/ResizableView/ResizableView.swift
@@ -11,15 +11,24 @@ struct ResizableView: View {
     @State private var transform = Transform() // 이후 binding으로 변경예정!
     private let content = RoundedRectangle(cornerRadius: 30.0)
     private let color = Color.red
-    
+
     var body: some View {
+        let dragGesture = DragGesture()
+            .onChanged { value in
+                transform.offset = value.translation
+                print("[onChange] \(value) > \(transform.offset)")
+            }
+
         content
             .frame(width: transform.size.width,
                    height: transform.size.height,
                    alignment: .center)
             .foregroundColor(color)
-//            .position(x: transform.size.width/2,
-//                      y: transform.size.height/2)
+            .offset(transform.offset)
+            .gesture(dragGesture)
+            .onTapGesture(perform: {
+                print("[!!!!!!!!!!!!] onTapGesture()")
+            })
     }
 }
 
