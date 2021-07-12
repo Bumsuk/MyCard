2063236baf457c01069b6d591d099d1489978719
brown <xenix72@gmail.com>
2021-07-12 18:17:34 +0900

    check

--- Shared/Util/Operators.swift
+++ Shared/Util/Operators.swift
@@ -8,6 +8,7 @@
 import Foundation
 import SwiftUI
 
+// 원래 이렇게 쉽게 연산자 재정의가 되는게 아니었던 것 같은디....
 func + (left: CGSize, right: CGSize) -> CGSize {
     CGSize(
         width: left.width + right.width,
--- Shared/View/ResizableView/ResizableView.swift
+++ Shared/View/ResizableView/ResizableView.swift
@@ -10,27 +10,44 @@ import SwiftUI
 struct ResizableView: View {
     @State private var transform = Transform() // 이후 binding으로 변경예정!
     @State private var previousOffset: CGSize = .zero
+    @State private var previousRotation: Angle = .zero
+    
     private let content = RoundedRectangle(cornerRadius: 30.0)
     private let color = Color.red
 
     var body: some View {
         let dragGesture = DragGesture()
             .onChanged { value in
+                transform.offset = value.translation + previousOffset
+                /*
                 transform.offset = CGSize(width: value.translation.width + previousOffset.width,
                                           height: value.translation.height + previousOffset.height)
+                */
                 print("[onChange] \(value) > \(transform.offset)")
             }
             .onEnded { value in
                 previousOffset = transform.offset
             }
+        let rotationGesture = RotationGesture()
+            .onChanged { angle in
+                transform.rotation += angle - previousRotation
+                previousRotation = angle
+            }
+            .onEnded { _ in
+                previousRotation = .zero
+            }
+        
 
         content
             .frame(width: transform.size.width,
                    height: transform.size.height,
                    alignment: .center)
             .foregroundColor(color)
+            
             .offset(transform.offset)
+            .rotationEffect(transform.rotation)
             .gesture(dragGesture)
+            .gesture(rotationGesture)
     }
 }
 
