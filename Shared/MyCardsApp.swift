af7a57e1627e88563f9ccda690ce859233108250
brown <xenix72@gmail.com>
2021-07-12 11:47:12 +0900

    initial commit

---
+++ Shared/MyCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCardsApp.swift
+//  Shared
+//
+//  Created by brown on 2021/07/12.
+//
+
+import SwiftUI
+
+@main
+struct MyCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
